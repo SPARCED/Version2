ef14add7e5e3b915fbabaa0a4eb889054901d685
chocolatecharlie <aurore.amrit@umu.se>
2026-07-30 15:22:18 +0200

    Retrieve nb_cells from context dictionnary

--- src/experiment_orchestrator/worker.py
+++ src/experiment_orchestrator/worker.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from config.mpi import BROKER_RANK
+from config.protocol_file_format import ProtocolContextKeys
 
 from runtime.logging import log, log_time
 from runtime.performance import now
@@ -13,7 +14,7 @@ def worker(comm, rank, size, context):
     log.info("Worker started")
     log.debug("Context: %r", context)
 
-    nb_cells = 10 # context[YAML_CELL_NUMBER]
+    nb_cells = context[ProtocolContextKeys.NB_CELLS] + 1
     nb_workers = size - 1
     for task_id in range(rank, nb_cells, nb_workers):
         log.debug("Worker takes cell: %d", task_id)
