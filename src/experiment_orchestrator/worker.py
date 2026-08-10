638753cbb63166d4d848757d9cf2e10312b6625e
chocolatecharlie <aurore.amrit@umu.se>
2026-08-10 12:51:22 +0200

    Outline worker's tasks

--- src/experiment_orchestrator/worker.py
+++ src/experiment_orchestrator/worker.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import deque
+
 from config.mpi import BROKER_RANK
 from config.protocol_file_format import ProtocolContextKeys
 
@@ -14,10 +16,49 @@ def worker(comm, rank, size, context):
     log.info("Worker started")
     log.debug("Context: %r", context)
 
-    nb_cells = context[ProtocolContextKeys.NB_CELLS] + 1
+    # Deduce from rank assigned list of cells
+    nb_cells = context[ProtocolContextKeys.NB_CELLS]
     nb_workers = size - 1
-    for task_id in range(rank, nb_cells, nb_workers):
-        log.debug("Worker takes cell: %d", task_id)
+
+    cell_ids = [c for c in range(rank, nb_cells+1, nb_workers)]
+    log.debug("Worker got assigned the following cells: %s", cell_ids)
+
+    # TODO: setup solver (reading SBML is dependent on chosen solver)
+    # Could still be nice to store initial state if possible
+
+    # Build protocol (create original FIFO)
+    protocol = []
+
+    for s in context[ProtocolContextKeys.PROTOCOL]:
+        log.debug("Worker loads step: %s", s)
+        step = 42 # Simulation(**s["step"])
+        # step.set_solver()? or pass it in run?
+        protocol.append(step)
+        # Step loaded as...
+
+        # Where to get data from: path + indication of retrieval
+        # Modifications to perform
+        # Simulation mode
+        # Time and step (start, stop, step)
+        # Storage path
+
+    # Per cell tasks
+    # Handle the FIFO
+    # Read next cell task ans launch simulator
+    # Handle simulator output if necessary
+
+    for cell in cell_ids:
+        log.debug("Worker takes cell: %d", cell)
+
+        protocol_queue = deque(protocol)    # WARNING: Shallow copy, always reset the objects afterwards
+
+        while protocol_queue:
+            simulation = protocol_queue.popleft()
+            #simulation.revolve_some_stuff() # Cell number, effectively grab the data
+            #simulation.run()    # Save included, every 100 timepoints
+            #status = simulation.status()
+            # if status blablabla -> queue.append()
+            # simulation.reset() !!! try finally
 
     t_worker_stop = now() - t_worker_start
     log_time("worker: %f", t_worker_stop)
